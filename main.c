2dfdec831e2723b19421e0fc6f4dc9021c642f32
OVHAY <98146728+OVHAY@users.noreply.github.com>
2022-11-14 21:15:08 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,86 +1,75 @@
-#include<stdio.h>
-#include<conio.h>
-#define st_size 5
-int item,top,st10[10];
-top=-1;
-void push()
-{
-    if(top==st_size)
-    printf("stack overflow");
-    else
-    {
-        top++;
-        st[top]=item;
-    }
-}
-void pop()
-{
-int del;
-if(top==-1)
-    printf("underflow");
-    else
-    {
-        del=st[top];
-        top--;
-        return del;
-    }
-}
-void display()
-{
-    int i;
-    if(top==-1)
-    printf("stack is empty \n nothing to show");
-    else
-    {
-        for(i=0;i<=top;i++)
-        printf("%d\t",st[i]);
-    }
-}
-void main()
-{
-    void push(void);
-void pop(void);
-void display(void);
-int main()
-{
-    printf("\n Enter the size of STACK[MAX=100]:");
-    scanf("%d",&n);
-    printf("\n\t STACK OPERATIONS USING ARRAY");
-    printf("\n\t--------------------------------");
-    printf("\n\t 1.PUSH\n\t 2.POP\n\t 3.DISPLAY\n\t 4.EXIT");
-    do
-    {
-        printf("\n Enter the Choice:");
-        scanf("%d",&choice);
-        switch(choice)
-        {
-            case 1:
-            {
-                push();
-                break;
-            }
-            case 2:
-            {
-                pop();
-                break;
-            }
-            case 3:
-            {
-                display();
-                break;
-            }
-            case 4:
-            {
-                printf("\n\t EXIT POINT ");
-                break;
-            }
-            default:
-            {
-                printf ("\n\t Please Enter a Valid Choice(1/2/3/4)");
-            }
-                
-        }
-    }
-    while(choice!=4);
-    return 0;
-}}
+#include<stdio.h> 
+ #include<stdlib.h> 
+  
+ #define stack_size 5 
+  
+ int st[10],item,top=-1; 
+  
+ void push() 
+ { 
+     if(top==stack_size) 
+     printf("\nOverflow"); 
+     else 
+     { 
+         top++; 
+         st[top]=item; 
+     } 
+ } 
+  
+ int pop() 
+ {   int del_item; 
+     if(top==-1) 
+     printf("\nUnderflow"); 
+     else 
+     { 
+         del_item=st[top]; 
+         top--; 
+     } 
+ return(del_item); 
+  
+ } 
+ void display() 
+ {   int i=0; 
+ if(top==-1) 
+ printf("\nUnderflow"); 
+ else     
+     { 
+         printf("\nStack:"); 
+     for(i=0;i<=top;i++) 
+         { 
+         printf("\t%d",st[i]); 
+         } 
+     } 
+ } 
+  
+ void main() 
+ {   int choice,l; 
+ while(1) 
+ { 
+  
+  
+     printf("\n\n\t---MENU---\n"); 
+     printf("1.Push\n2.Pop\n3.Display\n4.Exit\n"); 
+     scanf("%d",&choice); 
+  
+     switch(choice) 
+     { 
+         case 1: printf("Enter the element to be pushed:"); 
+                 scanf("%d",&item); 
+                 push(); 
+                 printf("Item has been pushed"); 
+                 break; 
+         case 2: l=pop(); 
+                 printf("Removed Item:%d",l); 
+                 break; 
+         case 3: display(); 
+                 break; 
+         case 4: exit(0); 
+                 break; 
+  
+  
+     } 
+  
+  
+ } 
+ }
